3c6410e69bb783a15b70f6c2c075525e17c5ec10
Irena Slahorkova <irena.slahorkova@wandera.com>
2021-05-23 23:41:29 +0200

    zakl. verze ukolu

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/repository/VizitkaRepository.java
@@ -0,0 +1,7 @@
+package cz.czechitas.java2webapps.ukol7.repository;
+
+import cz.czechitas.java2webapps.ukol7.entity.Vizitka;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VizitkaRepository extends CrudRepository<Vizitka, Integer> {
+}
